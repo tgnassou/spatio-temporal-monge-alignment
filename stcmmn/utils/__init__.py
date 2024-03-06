3c5fc9658ac92c373a8fc6ad6bf4d6116a9ce6a3
tgnassou <theo.gnassounou@gmail.com>
2024-03-06 14:10:39 +0100

    update cmmn

--- stcmmn/utils/__init__.py
+++ stcmmn/utils/__init__.py
@@ -1,4 +1,4 @@
-from ._cmmn import CMMN
+from ._cmmn import CMMN, get_csd
 from ._riemanian import RiemanianAlignment
 from ._data import load_BCI_dataset, load_sleep_dataset
 from ._architecture import AdaptiveShallowFBCSPNet
@@ -8,6 +8,7 @@ from ._functions import compute_final_conv_length
 from ._dataloader import DomainDataset, DomainBatchSampler
 
 __all__ = [
+    "get_csd",
     "CMMN",
     "RiemanianAlignment",
     "load_BCI_dataset",
--- stcmmn/utils/_cmmn.py
+++ stcmmn/utils/_cmmn.py
@@ -153,11 +153,11 @@ class CMMN(ABC):
     def _fixed_point_barycenter(self, B, psd):
         K = len(psd)
 
-        B_sqrt_ = self._matrix_operator(B.T, np.sqrt).T
+        B_sqrt_ = _matrix_operator(B.T, np.sqrt).T
         sum_B = np.einsum("ijl,njkl,kml -> niml", B_sqrt_, psd, B_sqrt_)
-        sum_B_sqrt = np.array([
-            self._matrix_operator(sum_B[i].T, np.sqrt).T for i in range(K)
-        ])
+        sum_B_sqrt = np.array(
+            [_matrix_operator(sum_B[i].T, np.sqrt).T for i in range(K)]
+        )
 
         # Compute the weights for each domain
         if self.weights is None:
@@ -207,9 +207,7 @@ class CMMN(ABC):
         if self.method == "spatio":
             m = np.zeros_like(psd[0][0])
             H = [
-                ot.gaussian.bures_wasserstein_mapping(
-                    m, m, psd[i], self.barycenter
-                )[0]
+                ot.gaussian.bures_wasserstein_mapping(m, m, psd[i], self.barycenter)[0]
                 for i in range(len(psd))
             ]
 
@@ -224,15 +222,11 @@ class CMMN(ABC):
         return H
 
     def _compute_spatio_temporal_filter(self, psd):
-        psd_sqrt = self._matrix_operator(psd.T, np.sqrt).T
-        psd_sqrt_inv = self._matrix_operator(psd.T, lambda x: 1 / np.sqrt(x)).T
-        D_ = np.einsum(
-            "ijl,jkl,kml -> iml", psd_sqrt, self.barycenter, psd_sqrt
-        )
-        D_sqrt_ = self._matrix_operator(D_.T, np.sqrt).T
-        D = np.einsum(
-            "ijl,jkl,kml -> iml", psd_sqrt_inv, D_sqrt_, psd_sqrt_inv
-        )
+        psd_sqrt = _matrix_operator(psd.T, np.sqrt).T
+        psd_sqrt_inv = _matrix_operator(psd.T, lambda x: 1 / np.sqrt(x)).T
+        D_ = np.einsum("ijl,jkl,kml -> iml", psd_sqrt, self.barycenter, psd_sqrt)
+        D_sqrt_ = _matrix_operator(D_.T, np.sqrt).T
+        D = np.einsum("ijl,jkl,kml -> iml", psd_sqrt_inv, D_sqrt_, psd_sqrt_inv)
         H = sp_fft.irfft(D, axis=-1)
         return H
 
@@ -251,48 +245,44 @@ class CMMN(ABC):
                 X_norm = self._temporal_convolution(X, H)
             else:
                 N = len(X)
-                X_norm = [
-                    self._temporal_convolution(X[i], H) for i in range(N)
-                ]
+                X_norm = [self._temporal_convolution(X[i], H) for i in range(N)]
 
         elif self.method == "spatiotemp":
             if self.concatenate_epochs:
                 X_norm = self._spatio_temporal_convolution(X, H)
             else:
                 N = len(X)
-                X_norm = [
-                    self._spatio_temporal_convolution(X[i], H)
-                    for i in range(N)
-                ]
+                X_norm = [self._spatio_temporal_convolution(X[i], H) for i in range(N)]
 
         elif self.method == "spatio":
             if self.concatenate_epochs:
                 X_norm = H @ X
             else:
                 N = len(X)
-                X_norm = [
-                    H @ X[i]
-                    for i in range(N)
-                ]
+                X_norm = [H @ X[i] for i in range(N)]
         return X_norm
 
     def _temporal_convolution(self, X, H):
         C = len(X)
-        X_norm = [
-            np.convolve(X[chan], H[chan], mode="same") for chan in range(C)
-        ]
+        X_norm = [np.convolve(X[chan], H[chan], mode="same") for chan in range(C)]
         X_norm = np.array(X_norm)
         return X_norm
 
-    def _spatio_temporal_convolution(self, X, H,):
+    def _spatio_temporal_convolution(
+        self,
+        X,
+        H,
+    ):
         C = len(X)
-        X_norm = np.array([
-            np.sum(
-                [np.convolve(X[j], H[i, j], mode="same") for j in range(C)],
-                axis=0,
-            )
-            for i in range(len(H))
-        ])
+        X_norm = np.array(
+            [
+                np.sum(
+                    [np.convolve(X[j], H[i, j], mode="same") for j in range(C)],
+                    axis=0,
+                )
+                for i in range(len(H))
+            ]
+        )
         return X_norm
 
     def compute_psd(self, X):
@@ -311,25 +301,30 @@ class CMMN(ABC):
         K = len(X)
         if self.method == "temp":
             if self.concatenate_epochs:
-                psd = [
-                    welch(X[i], nperseg=self.filter_size)[1]
-                    for i in range(K)
-                ]
+                psd = [welch(X[i], nperseg=self.filter_size)[1] for i in range(K)]
             else:
                 psd = []
                 for i in range(K):
                     # Compute the average power spectral density over the
                     # samples of the domain
                     N = len(X[i])
-                    psd.append(np.mean([
-                        welch(X[i][j], nperseg=self.filter_size)[1]
-                        for j in range(N)
-                    ], axis=0))
+                    psd.append(
+                        np.mean(
+                            [
+                                welch(X[i][j], nperseg=self.filter_size)[1]
+                                for j in range(N)
+                            ],
+                            axis=0,
+                        )
+                    )
 
         elif self.method == "spatiotemp":
             if self.concatenate_epochs:
                 psd = Parallel(n_jobs=self.n_jobs)(
-                    delayed(self._get_csd)(X[i]) for i in range(K)
+                    delayed(
+                        get_csd
+                    )(X[i], reg=self.reg, filter_size=self.filter_size)
+                    for i in range(K)
                 )
             else:
                 psd = []
@@ -337,27 +332,28 @@ class CMMN(ABC):
                     # Compute the average cross spectral density over the
                     # samples of the domain
                     N = len(X[i])
-                    psd.append(np.mean(
-                        [self._get_csd(X[i][j]) for j in range(N)],
-                        axis=0
-                    ))
+                    psd.append(
+                        np.mean(
+                            [
+                                get_csd(
+                                    X[i][j], reg=self.reg, filter_size=self.filter_size
+                                )
+                                for j in range(N)
+                            ],
+                            axis=0,
+                        )
+                    )
 
         elif self.method == "spatio":
             if self.concatenate_epochs:
-                psd = [
-                    np.cov(X[i])
-                    for i in range(K)
-                ]
+                psd = [np.cov(X[i]) for i in range(K)]
             else:
                 psd = []
                 for i in range(K):
                     # Compute the average covariance over the
                     # samples of the domain
                     N = len(X[i])
-                    psd.append(np.mean(
-                        [np.cov(X[i][j]) for j in range(N)],
-                        axis=0
-                    ))
+                    psd.append(np.mean([np.cov(X[i][j]) for j in range(N)], axis=0))
 
         return np.array(psd)
 
@@ -386,24 +382,50 @@ class CMMN(ABC):
             end += step
         return np.array(data)
 
-    def _get_csd(self, X):
-        psd = np.array([
-            csd(X, X[i], nperseg=self.filter_size)[1]
-            for i in range(len(X))
-        ])
-        # regularize the cross spectral density
-        psd = ledoit_wolf_shrinkage(psd, reg=self.reg)
-        return psd
 
-    def _matrix_operator(self, A, operator):
-        eigvals, eigvecs = np.linalg.eigh(A)
-        eigvals = np.expand_dims(eigvals, -2)
-        eigvals = operator(eigvals)
-        A_operator = (eigvecs * eigvals) @ np.swapaxes(eigvecs.conj(), -2, -1)
-        return A_operator
+def get_csd(X, reg=1e-7, filter_size=128):
+    psd = np.array([csd(X, X[i], nperseg=filter_size)[1] for i in range(len(X))])
+    # regularize the cross spectral density
+    psd = ledoit_wolf_shrinkage(psd, reg=reg)
+    return psd
+
+
+def _matrix_operator(A, operator):
+    eigvals, eigvecs = np.linalg.eigh(A)
+    eigvals = np.expand_dims(eigvals, -2)
+    eigvals = operator(eigvals)
+    A_operator = (eigvecs * eigvals) @ np.swapaxes(eigvecs.conj(), -2, -1)
+    return A_operator
 
 
 def ledoit_wolf_shrinkage(psd, reg=0.1):
     _, C, F = psd.shape
-    mu = np.sum(np.trace(psd, axis1=0, axis2=1)) / (C*F)
+    mu = np.sum(np.trace(psd, axis1=0, axis2=1)) / (C * F)
     return (reg * mu * np.eye(C) + (1 - reg) * psd.T).T
+
+
+def bure_wasserstein_distance(p, q,):
+    """Compute the Bure-Wasserstein distance between two covariance matrices.
+
+    Parameters
+    ----------
+    p : array, shape=(C, C)
+        Covariance matrix.
+    q : array, shape=(C, C)
+        Covariance matrix.
+
+    Returns
+    -------
+    float
+        Bure-Wasserstein distance.
+    """
+    p_sqrt = _matrix_operator(p.T, np.sqrt).T
+    D_ = np.einsum("ijl,jkl,kml -> iml", p_sqrt, q, p_sqrt)
+    D_sqrt_ = _matrix_operator(D_.T, np.sqrt).T
+
+    trace = 0
+    for i in range(len(p)):
+        trace -= 2 * np.sum(D_sqrt_[i, i])
+        trace += np.sum(p[i, i])
+        trace += np.sum(q[i, i])
+    return np.real(trace)
